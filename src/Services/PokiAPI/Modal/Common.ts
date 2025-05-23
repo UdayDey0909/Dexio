532ffe033718053fea83a115a6cd7ae4be3bb8a3
Uday Dey <udaydey0909@gmail.com>
2025-05-23 12:15:42 +0530

    Add react-native-svg dependency and define API resource interfaces

---
+++ src/Services/PokiAPI/Modal/Common.ts
@@ -0,0 +1,135 @@
+/**
+ * Standard resource with URL reference
+ */
+export interface APIResource {
+   /** URL pointing to the referenced resource */
+   url: string;
+}
+
+/**
+ * Standard named resource with name and URL reference
+ */
+export interface NamedAPIResource {
+   /** The name of the referenced resource */
+   name: string;
+
+   /** URL pointing to the referenced resource */
+   url: string;
+}
+
+/**
+ * Pagination result wrapper interface
+ */
+export interface PaginatedResults<T> {
+   /** The total number of resources available from this API */
+   count: number;
+
+   /** URL to the next page of resources (null if this is the last page) */
+   next: string | null;
+
+   /** URL to the previous page of resources (null if this is the first page) */
+   previous: string | null;
+
+   /** List of resources for the current page */
+   results: T[];
+}
+
+/**
+ * Type alias for a paginated list of named API resources
+ */
+export type NamedAPIResourceList = PaginatedResults<NamedAPIResource>;
+
+/**
+ * Description entry with language information
+ */
+export interface Description {
+   /** The localized description text */
+   description: string;
+
+   /** The language this description is in */
+   language: NamedAPIResource;
+}
+
+/**
+ * Effect entry with language information
+ */
+export interface Effect {
+   /** The localized effect text */
+   effect: string;
+
+   /** The language this effect is in */
+   language: NamedAPIResource;
+}
+
+/**
+ * Effect entry with short effect and language information
+ */
+export interface VerboseEffect {
+   /** The localized effect text */
+   effect: string;
+
+   /** The localized short effect text */
+   short_effect: string;
+
+   /** The language this effect is in */
+   language: NamedAPIResource;
+}
+
+/**
+ * Name entry with language information
+ */
+export interface Name {
+   /** The localized name */
+   name: string;
+
+   /** The language this name is in */
+   language: NamedAPIResource;
+}
+
+/**
+ * Flavor text entry with version and language information
+ */
+export interface FlavorText {
+   /** The localized flavor text */
+   flavor_text: string;
+
+   /** The language this flavor text is in */
+   language: NamedAPIResource;
+
+   /** The game version this flavor text is from */
+   version: NamedAPIResource;
+}
+
+/**
+ * Game index information with version reference
+ */
+export interface VersionGameIndex {
+   /** The internal ID of the game index */
+   game_index: number;
+
+   /** The version this game index is relevant to */
+   version: NamedAPIResource;
+}
+
+/**
+ * Language information
+ */
+export interface Language {
+   /** The identifier for this language resource */
+   id: number;
+
+   /** The name of the language */
+   name: string;
+
+   /** Whether this language is considered an official language */
+   official: boolean;
+
+   /** The ISO639 code for this language */
+   iso639: string;
+
+   /** The ISO3166 code for this language */
+   iso3166: string;
+
+   /** The localized names of this language, listed in different languages */
+   names: Name[];
+}
