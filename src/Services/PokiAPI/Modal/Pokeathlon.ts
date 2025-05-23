d0d172ff7bb4620f068e2ae5ecf07dc0f6b48db8
Uday Dey <udaydey0909@gmail.com>
2025-05-23 20:39:14 +0530

    Add PokeathlonStat interface and related structures for Pokémon performance in Pokéathlon events

---
+++ src/Services/PokiAPI/Modal/Pokeathlon.ts
@@ -0,0 +1,53 @@
+import { NamedAPIResource } from "./Common";
+
+/**
+ * Pokéathlon stats are different attributes of a Pokémon's performance in Pokéathlon events
+ *
+ * @endpoint https://pokeapi.co/api/v2/pokeathlon-stat/{id or name}/
+ */
+export interface PokeathlonStat {
+   /** The identifier for this Pokéathlon stat resource */
+   id: number;
+
+   /** The name for this Pokéathlon stat resource */
+   name: string;
+
+   /** The name of this Pokéathlon stat listed in different languages */
+   names: Name[];
+
+   /** A detail of natures which affect this Pokéathlon stat positively or negatively */
+   affecting_natures: NaturePokeathlonStatAffectSets;
+}
+
+/**
+ * Nature Pokéathlon stat affect sets
+ */
+export interface NaturePokeathlonStatAffectSets {
+   /** A list of natures and how they change the referenced Pokéathlon stat */
+   increase: NaturePokeathlonStatAffect[];
+
+   /** A list of natures and how they change the referenced Pokéathlon stat */
+   decrease: NaturePokeathlonStatAffect[];
+}
+
+/**
+ * Nature Pokéathlon stat effects
+ */
+export interface NaturePokeathlonStatAffect {
+   /** The maximum amount of change to the referenced Pokéathlon stat */
+   max_change: number;
+
+   /** The nature causing the change */
+   nature: NamedAPIResource;
+}
+
+/**
+ * Localized names used for various resources
+ */
+export interface Name {
+   /** The localized name */
+   name: string;
+
+   /** The language this name is in */
+   language: NamedAPIResource;
+}
